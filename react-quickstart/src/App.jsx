e63e93979c37663b93f174ce61a602af7f332794
aliciajane-bits <aliciajanemedina881@gmail.com>
2024-10-14 20:36:30 +0800

    I'm done completing the section on Displaying data.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,10 +1,22 @@
-function AboutPage() {
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
   return (
     <>
-      <h1>I'm adding styles bcoz I can</h1>
-      <img className="avatar" />
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
   );
 }
-
-export default AboutPage
